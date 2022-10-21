3a90c1e51e2ed4d1809add36578357c0f962a5fe
Raul Diamantino <rauldiamantino25@gmail.com>
2022-10-21 18:46:21 -0300

    Conclusão Banner Rotativo e mais...
    Banner Rotativo
    Menu Responsivo

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,21 @@
-let mainBanner = document.querySelector(".main-banner");
-let buttonRight = clickSideMainBanner(document.querySelector(".button-right"));
-let buttonLeft = clickSideMainBanner(document.querySelector(".button-left"));
+// Script MAIN BANNER
+// MENU RESPONSIVE
+let buttonOpen = document.querySelector(".button-responsive");
+let buttonClose = document.querySelector(".titleMenu");
+let menuCategories = document.querySelector(".background-categories");
 
-function clickSideMainBanner(button) {
-  button.addEventListener("click", () => {
-    verifyButton(button);
-  });
+buttonOpen.addEventListener("click", function () {
+  openMenu();
+});
+
+buttonClose.addEventListener("click", function () {
+  closeMenu();
+});
+
+function openMenu() {
+  menuCategories.classList.add("open");
 }
 
-function verifyButton(button) {
-  if (button.classList.contains("button-right")) {
-    rightSideBanner();
-  } else {
-    leftSideBanner();
-  }
-}
-
-function rightSideBanner() {
-  if (mainBanner.classList.contains("banner1")) {
-    mainBanner.classList.add("banner2");
-    mainBanner.classList.remove("banner1");
-  } else if (mainBanner.classList.contains("banner2")) {
-    mainBanner.classList.add("banner3");
-    mainBanner.classList.remove("banner2");
-  } else if (mainBanner.classList.contains("banner3")) {
-    mainBanner.classList.add("banner1");
-    mainBanner.classList.remove("banner3");
-  }
-}
-
-function leftSideBanner() {
-  if (mainBanner.classList.contains("banner3")) {
-    mainBanner.classList.add("banner2");
-    mainBanner.classList.remove("banner3");
-  } else if (mainBanner.classList.contains("banner2")) {
-    mainBanner.classList.add("banner1");
-    mainBanner.classList.remove("banner2");
-  } else if (mainBanner.classList.contains("banner1")) {
-    mainBanner.classList.add("banner3");
-    mainBanner.classList.remove("banner1");
-  }
+function closeMenu() {
+  menuCategories.classList.remove("open");
 }

e0f24b5b9e95aec0c8855f6d1a78d532619c5f8f
Raul Diamantino <rauldiamantino25@gmail.com>
2022-12-14 19:50:31 -0300

    Bug correction to open src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-     content: ["./**/*.{html,js}"],
+     content: ["./*.{html,js}"],
      theme: {
           extend: {
                colors: {
                     "c-dark-gray": "#494949",
                },
                translate: {
-                '150%': '150%',
-              }
+                    "translate-150%": "150%",
+               },
           },
      },
      plugins: [],
